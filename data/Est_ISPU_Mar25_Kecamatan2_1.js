cb105a163d2cf00bb5f4268c2572599906ae1b40
MUHAMMAD RAFI TASRIF <222112219@stis.ac.id>
2025-08-01 02:40:00 +0700

    hapus file data

--- data/Est_ISPU_Mar25_Kecamatan2_1.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:fe8ad3e211d76e2c5b353206891b6bb73e35dfa8c6c8ea2f03a86ed9ab6e4e06
-size 275129467
--- data/Est_ISPU_Mar25_Provinsi3_2.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:b71476cd0d09626bd18aec287dd9285dfc60db3e41ef9b2ae96d386edea7d3c2
-size 96498474
