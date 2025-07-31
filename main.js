ad3f5b5ce2e2a14ab9a16f25af0cc33d6953609c
MUHAMMAD RAFI TASRIF <222112219@stis.ac.id>
2025-08-01 02:17:30 +0700

    hapus komen https pada mainjs

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f69abe08993984101c1c9fc5856cbedb47b417bda7e86914bd23aa458c4c8ce4
-size 4883
+oid sha256:d5787e793b8cbb6ab18b96d0c332125e91f608d657eca1e76e2a0ef269abe63a
+size 4645
