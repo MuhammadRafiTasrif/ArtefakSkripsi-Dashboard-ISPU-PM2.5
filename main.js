f647f289bec65f8864a1a5b35c19002a696185d1
MUHAMMAD RAFI TASRIF <222112219@stis.ac.id>
2025-08-01 02:32:05 +0700

    Remove main.js from LFS tracking and addactualfile

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c60d3daab42b4417e10a35758af201b0311de7914021a0ac670b8d69fbdba502
-size 4647
+oid sha256:905398b726b905b9b18c22362f4b98ab69ea7733a950cbf2d95a5cf302ddfc96
+size 5137
